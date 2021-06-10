f523897f0594418fc3cb31a2360ebf27db9e7adc
Kialbe <k14lb3@gmail.com>
2021-06-11 06:41:06 +0800

    Add Register component

---
+++ src/components/Register.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { COLORS } from 'colors';
+import Heading from 'components/Heading';
+import Label from 'components/Label';
+import InputText from 'components/InputText';
+import InputPassword from 'components/InputPassword';
+import Button from 'components/Button';
+import Link from 'components/Link';
+
+const Window = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+  border: solid 1px ${COLORS.WHITE};
+`;
+
+const Register = () => {
+  return (
+    <main>
+      <Window>
+        <Heading addStyle="margin-bottom: 1em;">Create an account</Heading>
+        <Label>Email</Label>
+        <InputText addStyle="margin-bottom: 1em" />
+        <Label>Username</Label>
+        <InputText addStyle="margin-bottom: 1em" />
+        <Label>Password</Label>
+        <InputPassword addStyle="margin-bottom: 1em" />
+        <Button addStyle="margin-bottom: 0.5em;">Register</Button>
+        <Link to="/login">Already have an account?</Link>
+      </Window>
+    </main>
+  );
+};
+
+export default Register;
