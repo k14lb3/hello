e971ad5cf2874622e12b52dbea2fa1d75a34ae24
Kialbe <k14lb3@gmail.com>
2021-06-11 06:08:11 +0800

    Add Link component

---
+++ src/components/Link.js
@@ -0,0 +1,17 @@
+import { Link as ReactLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { COLORS } from 'colors';
+
+const StyledLink = styled(ReactLink)`
+  color: ${COLORS.WHITE};
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Link = ({ children, ...rest }) => {
+  return <StyledLink {...rest}>{children}</StyledLink>;
+};
+
+export default Link;
