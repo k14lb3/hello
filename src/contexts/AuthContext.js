3c130fbf15f000fde0b29df1d65b22ef8ce907c8
Kialbe <k14lb3@gmail.com>
2021-06-11 19:49:13 +0800

    Add AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import { db, auth } from 'firebase.js';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, []);
+
+  const value = {
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
