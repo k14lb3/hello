e5072671633bea400dc7b69ec1ada72695f5c2c2
Kialbe <k14lb3@gmail.com>
2021-06-12 02:30:04 +0800

    Add register function

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext, createContext } from 'react';
-import { db, auth } from 'firebase.js';
+import { useState, useEffect, useContext, createContext } from 'react';
+import { auth, db } from 'firebase.js';
 
 const AuthContext = createContext();
 
@@ -8,6 +8,19 @@ export const useAuth = () => useContext(AuthContext);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(true);
+
+  const register = async (email, username, password) => {
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      throw new Error('Invalid email address format');
+    }
+    if (username.length < 6 || username.length > 12) {
+      throw new Error('Username must be at least 6 - 12 characters long');
+    }
+    if (password.length < 8 || password.length > 32) {
+      throw new Error('Password must be at least 8 - 32 characters long');
+    }
+    await auth.createUserWithEmailAndPassword(email, password);
+  }; //
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -18,6 +31,7 @@ const AuthProvider = ({ children }) => {
 
   const value = {
     user,
+    register,
   };
 
   return (
