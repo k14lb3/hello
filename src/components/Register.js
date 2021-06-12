beaca881d2030967895b6f9ab02a5104cb483c58
Kialbe <k14lb3@gmail.com>
2021-06-12 17:11:06 +0800

    Surround the Input and Label components with Form component because chrome told me to lol

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,10 +10,13 @@ import Button from 'components/Button';
 import Link from 'components/Link';
 
 const Window = styled.div`
-  display: flex;
-  flex-direction: column;
   padding: 2em;
   border: solid 1px ${COLORS.WHITE};
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Register = () => {
@@ -35,18 +38,32 @@ const Register = () => {
     <main>
       <Window>
         <Heading addStyle="margin-bottom: 1em;">create an account</Heading>
-        <Label>email</Label>
-        <InputText ref={emailRef} addStyle="margin-bottom: 1em;" />
-        <Label>username</Label>
-        <InputText ref={usernameRef} addStyle="margin-bottom: 1em;" />
-        <Label>password</Label>
-        <InputPassword ref={passwordRef} addStyle="margin-bottom: 1em;" />
-        <Button addStyle="margin-bottom: 0.5em;" onClick={handleRegister}>
-          register
-        </Button>
-        <span>
-          <Link to="/login">already have an account?</Link>
-        </span>
+        <Form>
+          <Label>email</Label>
+          <InputText
+            ref={emailRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-email"
+          />
+          <Label>username</Label>
+          <InputText
+            ref={usernameRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-username"
+          />
+          <Label>password</Label>
+          <InputPassword
+            ref={passwordRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-password"
+          />
+          <Button addStyle="margin-bottom: 0.5em;" onClick={handleRegister}>
+            register
+          </Button>
+          <span>
+            <Link to="/login">already have an account?</Link>
+          </span>
+        </Form>
       </Window>
     </main>
   );
