4b6d4091b079e161b4afbd1db3ca180131732391
Kialbe <k14lb3@gmail.com>
2021-06-12 17:45:20 +0800

    Move components to containers folder (forgot to add this in the previous commit)

---
+++ src/containers/Login.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { COLORS } from 'colors';
+import Heading from 'components/Heading';
+import Label from 'components/Label';
+import InputText from 'components/InputText';
+import Button from 'components/Button';
+import Link from 'components/Link';
+
+const Window = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+  border: solid 1px ${COLORS.WHITE};
+`;
+
+const Login = () => {
+  return (
+    <main>
+      <Window>
+        <Heading addStyle="margin-bottom: 1em;">hello mah prend</Heading>
+        <Label>username</Label>
+        <InputText addStyle="margin-bottom: 1em;" />
+        <Label>password</Label>
+        <InputText addStyle="margin-bottom: .5em;" />
+        <span>
+          <Link to="/">forgot your password?</Link>
+        </span>
+        <Button addStyle="margin-top: 1em; margin-bottom: 0.5em;">login</Button>
+        <span>
+          need an account? <Link to="/register">register</Link>
+        </span>
+      </Window>
+    </main>
+  );
+};
+
+export default Login;
---
+++ src/containers/Register.js
@@ -0,0 +1,72 @@
+import { useState, useRef } from 'react';
+import styled from 'styled-components';
+import { useAuth } from 'contexts/AuthContext';
+import { COLORS } from 'colors';
+import Heading from 'components/Heading';
+import Label from 'components/Label';
+import InputText from 'components/InputText';
+import InputPassword from 'components/InputPassword';
+import Button from 'components/Button';
+import Link from 'components/Link';
+
+const Window = styled.div`
+  padding: 2em;
+  border: solid 1px ${COLORS.WHITE};
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Register = () => {
+  const { register } = useAuth();
+  const emailRef = useRef();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleRegister = async () => {
+    const email = emailRef.current.value;
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    try {
+      await register(email, username, password);
+    } catch (err) {}
+  };
+
+  return (
+    <main>
+      <Window>
+        <Heading addStyle="margin-bottom: 1em;">create an account</Heading>
+        <Form>
+          <Label>email</Label>
+          <InputText
+            ref={emailRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-email"
+          />
+          <Label>username</Label>
+          <InputText
+            ref={usernameRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-username"
+          />
+          <Label>password</Label>
+          <InputPassword
+            ref={passwordRef}
+            addStyle="margin-bottom: 1em;"
+            autoComplete="new-password"
+          />
+          <Button addStyle="margin-bottom: 0.5em;" onClick={handleRegister}>
+            register
+          </Button>
+          <span>
+            <Link to="/login">already have an account?</Link>
+          </span>
+        </Form>
+      </Window>
+    </main>
+  );
+};
+
+export default Register;
